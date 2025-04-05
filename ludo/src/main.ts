6c716fd4a3c7a0673ab0326b663eeb96dfbcf490
kadar naruto <khadarpaashe123@gmail.com>
2025-04-05 08:34:55 +0300

    setted up eslint

--- ludo/src/main.ts
+++ ludo/src/main.ts
@@ -1,24 +1,14 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+///////////////// selectors
+const dice = document?.querySelector<HTMLImageElement>(".dice");
+const roll = document?.querySelector<HTMLDivElement>(".btn--roll");
+const hold = document?.querySelector<HTMLDivElement>(".btn--hold");
+console.log(dice);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// ROLL DICE
+const rollDice = function () {
+  const roll = Math.trunc(Math.random() * 6 + 1);
+  console.log(roll);
+  return roll;
+};
